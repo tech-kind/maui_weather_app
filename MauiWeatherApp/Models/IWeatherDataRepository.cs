a1141b85cd5285e423ce8b1b0672dcaff03df894
tech-kind <ken_hayakawa@tech-kind.biz>
2022-10-12 14:20:22 +0900

    モデルの追加

---
+++ MauiWeatherApp/Models/IWeatherDataRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiWeatherApp.Models;
+
+public interface IWeatherDataRepository
+{
+    Task<WeatherData> GetAllWeatherDataAsync();
+    Task<List<Hourly>> GetHourliesAsync();
+    Task<List<Daily>> GetDaysAsync();
+    Task<Placemark> GetPlacemarkAsync();
+}
---
+++ MauiWeatherApp/Models/WeatherDataRepository.cs
@@ -0,0 +1,87 @@
+﻿using MauiWeatherApp.Constants;
+using System.Text.Json;
+
+namespace MauiWeatherApp.Models;
+
+public class WeatherDataRepository : IWeatherDataRepository
+{
+    private readonly HttpClient _httpClient;
+
+    public WeatherDataRepository()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<WeatherData> GetAllWeatherDataAsync()
+    {
+        return await GetAsyncMethodFactory<WeatherData>(TypeMethod.GetAllWeatherDataAsync);
+    }
+
+    public async Task<List<Daily>> GetDaysAsync()
+    {
+        return await GetAsyncMethodFactory<List<Daily>>(TypeMethod.GetDaysAsync);
+    }
+
+    public async Task<List<Hourly>> GetHourliesAsync()
+    {
+        return await GetAsyncMethodFactory<List<Hourly>>(TypeMethod.GetHouriesAsync);
+    }
+
+    public async Task<Placemark> GetPlacemarkAsync()
+    {
+        Location location = await Geolocation.Default.GetLocationAsync();
+
+        IEnumerable<Placemark> placemarks = await Geocoding.Default.GetPlacemarksAsync(location.Latitude, location.Longitude);
+        Placemark placemark = placemarks?.FirstOrDefault();
+
+        return placemark;
+    }
+
+    public async Task<T> GetAsyncMethodFactory<T>(TypeMethod typeMethod) where T : class
+    {
+        Location location = await Geolocation.Default.GetLocationAsync();
+        Uri url = new(GenerateRequestUri(APIConstants.OpenWeatherMapEndpoint, location.Longitude, location.Latitude));
+
+        HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            return GetDeserializedContent<T>(typeMethod, content);
+        }
+        return null;
+    }
+
+    public static T GetDeserializedContent<T>(TypeMethod typeMethod, string content) where T : class
+    {
+        switch (typeMethod)
+        {
+            case TypeMethod.GetAllWeatherDataAsync:
+                return JsonSerializer.Deserialize<WeatherData>(content) as T;
+            case TypeMethod.GetHouriesAsync:
+                return JsonSerializer.Deserialize<WeatherData>(content).Hourly as T;
+            case TypeMethod.GetDaysAsync:
+                return JsonSerializer.Deserialize<WeatherData>(content).Daily as T;
+            default:
+                throw new ArgumentException($"Invalid method type: {typeMethod}");
+        }
+    }
+
+    private string GenerateRequestUri(string endpoint, double latitude, double longitude)
+    {
+        string requestUri = endpoint;
+        requestUri += $"?lat={latitude}";
+        requestUri += $"&lon={longitude}";
+        requestUri += "&exclude=current,minutely,alerts&units=metric";
+        requestUri += $"&appid={APIConstants.OpenWeatherMapAPIKey}";
+
+        return requestUri;
+    }
+}
+
+public enum TypeMethod
+{
+    GetAllWeatherDataAsync,
+    GetHouriesAsync,
+    GetDaysAsync
+}
